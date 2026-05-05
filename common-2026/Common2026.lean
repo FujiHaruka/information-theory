186e52f6d6c937cddb5ec414c2975f916db9a970
FujiHaruka <fizzyjohn1427@gmail.com>
2026-05-05 21:12:00 +0900

    Init common-2026

---
+++ common-2026/Common2026.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Common2026` library.
+-- Import modules here that should be built as part of the library.
+import Common2026.Basic
---
+++ common-2026/Main.lean
@@ -0,0 +1,4 @@
+import Common2026
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
